65ab55b595c8ee1f0f7730f75fe3e441009f0c72
Malakai Ricketts <codedbymalakai@gmail.com>
2026-01-28 17:18:22 +0000

    Changed the homepage to just a log in button and dashboard button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import { ComponentExample } from "@/components/component-example";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <main className="min-h-screen flex items-center justify-center gap-3">
+      {/* Link is Next's client-side navigation */}
+      <Link href="/login">
+        <Button>Login</Button>
+      </Link>
+
+      <Link href="/dashboard">
+        <Button variant="outline">Dashboard</Button>
+      </Link>
+    </main>
+  );
 }
